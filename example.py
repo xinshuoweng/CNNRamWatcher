8cd5766d1680f714038b3df45893a68c2927ad27
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-02 15:51:41 -0400

    update example

--- example.py
+++ example.py
@@ -19,7 +19,11 @@ def main():
 	network.append(Convolution(name='conv3', nOutputPlane=1024, kernal_size=3, padding=1))
 	network.append(Pooling(name='pool3', kernal_size=2, stride=2))
 	network.compile(np.ndarray((1, 384, 256, 3)))
-	network.summary(savepath='/home/xinshuo/summary.png')
+
+	# save network info to a table and also the memory usage chart to an image
+	network_table_save_path = 'table.txt'
+	network_pie_chart_save_path = 'chart.png'
+	network.summary(table_path=network_table_save_path, chart_path=network_pie_chart_save_path)
 
 
 
